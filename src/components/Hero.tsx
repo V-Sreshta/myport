169d28ddcc3a657ef0f7e65af4a7ea0e134139a5
V-Sreshta <vaddesreshta133@gmail.com>
2025-07-02 16:45:11 +0530

    1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -85,22 +85,6 @@ const Hero = () => {
             </a>
           </motion.div>
         </div>
-
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5 }}
-          className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-        >
-          <a href="#profile" className="flex flex-col items-center text-gray-600 dark:text-gray-400 hover:text-orange-500 dark:hover:text-orange-400 transition-colors">
-            <span className="text-sm mb-2">Scroll down</span>
-            <motion.div
-              animate={{ y: [0, 10, 0] }}
-              transition={{ duration: 2, repeat: Infinity }}
-            >
-              <ChevronDown size={24} />
-            </motion.div>
-          </a>
         </motion.div>
       </div>
     </section>
